f378905d6bb2c78d4c05b0cc243043d937d57e99
askeynil <askeynil@gmail.com>
2020-08-19 18:29:10 +0800

    feat: 添加版本参数

--- install_nvm.sh
+++ install_nvm.sh
@@ -2,5 +2,8 @@
 
 bash -c "$(curl -fsSL https://cdn.jsdelivr.net/gh/nvm-sh/nvm@0.35.3/install.sh | sed 's/https:\/\/raw.githubusercontent.com\/nvm-sh\/nvm\//https:\/\/cdn.jsdelivr.net\/gh\/nvm-sh\/nvm@/g' | sed 's/https:\/\/github.com\/nvm-sh\/nvm.git/https:\/\/github.com.cnpmjs.org\/nvm-sh\/nvm.git/g' )"
 
-
-nvm install `python read_json.py nodejs_version`
+if [ "$1" = "" ]; then 
+    nvm install `python read_json.py nodejs_version`
+else
+    nvm install "$1"
+fi
